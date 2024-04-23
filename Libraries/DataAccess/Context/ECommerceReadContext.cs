5f522fa3451d69d9ce594da4d3af26e9d51f786c
semih <semihotun1@gmail.com>
2024-04-23 20:46:22 +0300

    Remove All dbset

--- Libraries/DataAccess/Context/ECommerceReadContext.cs
+++ Libraries/DataAccess/Context/ECommerceReadContext.cs
@@ -1,7 +1,7 @@
 ﻿using Core.SeedWork;
-using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Reflection;
 
 namespace DataAccess.Context
 {
@@ -10,39 +10,13 @@ namespace DataAccess.Context
         public ECommerceReadContext(DbContextOptions<ECommerceReadContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
         public IQueryable<TEntity> Query<TEntity>() where TEntity : BaseEntity
         {
             return Set<TEntity>().AsQueryable().Where(x => !x.IsDeleted);
         }
-        #region DBSets
-        public DbSet<AdminUser> AdminUser { get; set; }
-        public DbSet<ProductLike> ProductLike { get; set; }
-        public DbSet<Address> Addres { get; set; }
-        public DbSet<ProductShipmentInfo> ProductShipmentInfo { get; set; }
-        public DbSet<Brand> Brand { get; set; }
-        public DbSet<CatalogBrand> CatalogBrand { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<PredefinedProductAttributeValue> PredefinedProductAttributeValue { get; set; }
-        public DbSet<Product> Product { get; set; }
-        public DbSet<ProductAttributeMapping> ProductAttributeMapping { get; set; }
-        public DbSet<ProductAttribute> ProductAttribute { get; set; }
-        public DbSet<ProductAttributeCombination> ProductAttributeCombination { get; set; }
-        public DbSet<ProductAttributeValue> ProductAttributeValue { get; set; }
-        public DbSet<ProductPhoto> ProductPhoto { get; set; }
-        public DbSet<ProductSeo> ProductSeo { get; set; }
-        public DbSet<ShowCase> ShowCase { get; set; }
-        public DbSet<Slider> Slider { get; set; }
-        public DbSet<ShowCaseProduct> ShowCaseProduct { get; set; }
-        public DbSet<CombinationPhoto> CombinationPhoto { get; set; }
-        public DbSet<CategorySpefication> CategorySpefication { get; set; }
-        public DbSet<SpecificationAttribute> SpecificationAttribute { get; set; }
-        public DbSet<SpecificationAttributeOption> SpecificationAttributeOption { get; set; }
-        public DbSet<ProductSpecificationAttribute> ProductSpecificationAttribute { get; set; }
-        public DbSet<Comment> Comment { get; set; }
-        public DbSet<ShowCaseType> ShowCaseType { get; set; }
-        public DbSet<ProductStock> ProductStock { get; set; }
-        public DbSet<ProductStockType> ProductStockType { get; set; }
-        public DbSet<CustomerUser> CustomerUser { get; set; }
-        #endregion
     }
 }
