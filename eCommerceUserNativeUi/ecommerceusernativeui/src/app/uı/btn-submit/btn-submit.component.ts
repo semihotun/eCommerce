20ccf737d09156ec8546f7d993b991582db606f7
semih <semihotun1@gmail.com>
2024-07-11 20:21:50 +0300

    Theme:button submit

---
+++ eCommerceUserNativeUi/ecommerceusernativeui/src/app/uı/btn-submit/btn-submit.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-submit',
+  templateUrl: './btn-submit.component.html',
+  styleUrls: ['./btn-submit.component.scss'],
+  standalone: true,
+})
+export class BtnSubmitComponent implements OnInit {
+  constructor() {}
+
+  @Input() text: string = '';
+  @Output() click: EventEmitter<any> = new EventEmitter<any>();
+  ngOnInit() {}
+  btnClick() {
+    this.click.emit();
+  }
+}
