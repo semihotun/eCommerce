dc40df1c819ea5444a11aa5bd0be816f1b8da9e8
semih <semihotun1@gmail.com>
2024-07-11 19:15:41 +0300

    replace filepath

--- eCommerceUserNativeUi/ecommerceusernativeui/src/app/app.routes.ts
+++ eCommerceUserNativeUi/ecommerceusernativeui/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './screens/login/login.page';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
       import('./screens/home/home.page').then((m) => m.HomePage),
+    pathMatch: 'full',
   },
   {
     path: '',
@@ -15,21 +17,25 @@ export const routes: Routes = [
     path: 'basket',
     loadComponent: () =>
       import('./screens/basket/basket.page').then((m) => m.BasketPage),
+    pathMatch: 'full',
   },
   {
     path: 'catalog',
     loadComponent: () =>
       import('./screens/catalog/catalog.page').then((m) => m.CatalogPage),
+    pathMatch: 'full',
   },
   {
     path: 'comments',
     loadComponent: () =>
       import('./screens/comments/comments.page').then((m) => m.CommentsPage),
+    pathMatch: 'full',
   },
   {
     path: 'login',
     loadComponent: () =>
       import('./screens/login/login.page').then((m) => m.LoginPage),
+    pathMatch: 'full',
   },
   {
     path: 'product-detail',
@@ -37,10 +43,12 @@ export const routes: Routes = [
       import('./screens/product-detail/product-detail.page').then(
         (m) => m.ProductDetailPage
       ),
+    pathMatch: 'full',
   },
   {
     path: 'register',
     loadComponent: () =>
       import('./screens/register/register.page').then((m) => m.RegisterPage),
+    pathMatch: 'full',
   },
 ];
